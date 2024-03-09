f640a1229b2a9b264f4a29f152db8f26c4a0364d
Wasim Raja <wasim7raja10@gmail.com>
2024-03-09 20:20:29 +0530

    change default style

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,9 +1,8 @@
 <script>
 	import Button from "$lib/components/ui/button/button.svelte";
-
 </script>
 
 <header>
 	<h1 class="text-blue-500">Duggup</h1>
-	<Button variant="destructive">Hello</Button>
+	<Button>Button</Button>
 </header>
