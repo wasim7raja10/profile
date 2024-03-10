1cffbf7ecadecb8c7741b0913e896934f3a61b3e
Wasim Raja <wasim7raja10@gmail.com>
2024-03-10 21:26:25 +0530

    Added navigation link

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -8,11 +8,18 @@
 			<img height="27" width="108" src="src/lib/images/brand.svg" alt="Logo Brand" />
 
 			<div class="flex items-center gap-10">
-				<div>Home</div>
-				<div>Home</div>
-				<div>Home</div>
-				<div>Home</div>
-				<div>Home</div>
+				<a href="/home" class="text-foreground-secondary flex items-center gap-2">
+					<img class="m-1 h-4 w-4" src="src/lib/icons/home.svg" alt="home icon" />
+					<span class="text-sm leading-5">Home</span>
+				</a>
+				<a href="/home" class="text-foreground-secondary flex items-center gap-2">
+					<img class="m-1 h-4 w-4" src="src/lib/icons/explore.svg" alt="explore icon" />
+					<span class="text-sm leading-5">Explore</span>
+				</a>
+				<a href="/home" class="text-foreground-secondary flex items-center gap-2">
+					<img class="m-1 h-4 w-4" src="src/lib/icons/learn.svg" alt="learn icon" />
+					<span class="text-sm leading-5">Learn</span>
+				</a>
 			</div>
 		</div>
 	</Container>
