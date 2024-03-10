73d8dfbff59dc65a96c0823fd805d75b7f51c5ce
Wasim Raja <wasim7raja10@gmail.com>
2024-03-10 23:50:59 +0530

    header done

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,1 +1,7 @@
-<div class="container mx-auto px-5"><slot/></div>
+<script>
+	import { cn } from '$lib/utils.js';
+
+	export let classes = '';
+</script>
+
+<div class={cn('container mx-auto px-5', classes)}><slot /></div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,14 +1,16 @@
 <script>
 	import Container from '$lib/components/Container.svelte';
 	import CtaButton from '$lib/components/CtaButton.svelte';
+	import DottedGradient from '$lib/components/DottedGradient.svelte';
 	import Explore from '$lib/components/icons/Explore.svelte';
 	import Home from '$lib/components/icons/Home.svelte';
-	import Learn from '$lib/components/icons/Learn.svelte';
 	import Plus from '$lib/components/icons/Plus.svelte';
+	import * as Avatar from '$lib/components/ui/avatar/index';
+	import { ChevronDown, BookOpen } from 'lucide-svelte';
 </script>
 
-<header class="py-4">
-	<Container>
+<header>
+	<Container classes="py-4">
 		<div class="flex items-center justify-between">
 			<img height="27" width="108" src="src/lib/images/brand.svg" alt="Logo Brand" />
 
@@ -21,15 +23,25 @@
 					<Explore />
 					<span class="text-sm leading-5 group-hover:text-hover">Explore</span>
 				</a>
-				<a href="/home" class="group flex items-center gap-2">
-					<Learn />
-					<span class="text-sm leading-5 group-hover:text-hover">Learn</span>
+				<a href="/home" class="group flex items-center gap-2 hover:text-hover">
+					<BookOpen size="16" class="mt-1" />
+					<span class="text-sm">Learn</span>
 				</a>
 				<CtaButton>
 					<Plus />
 					Post
 				</CtaButton>
+
+				<div class="flex items-center gap-3 px-4">
+					<Avatar.Root class="h-12 w-12">
+						<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+						<Avatar.Fallback>KK</Avatar.Fallback>
+					</Avatar.Root>
+					<h2 class="text-xl font-medium">Krishna Kiran</h2>
+					<ChevronDown size="16" />
+				</div>
 			</div>
 		</div>
 	</Container>
+	<Container><DottedGradient /></Container>
 </header>
